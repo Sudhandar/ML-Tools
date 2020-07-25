269c1e4f9435439419f71b16feac537859179678
Sudhandar <sudhandar@gmail.com>
2020-07-25 12:38:57 +0530

    univariate feature selection

---
+++ univariate_feature_selection.py
@@ -0,0 +1,45 @@
+from sklearn.feature_selection import chi2
+from sklearn.feature_selection import f_classif
+from sklearn.feature_selection import f_regression
+from sklearn.feature_selection import mutual_info_classif
+from sklearn.feature_selection import mutual_info_regression
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import SelectPercentile
+
+class UnivariateFeatureSelection():
+    def __init__(self, n_features, problem_type, scoring):
+        """ 
+        Custom univariate feature selection wrapper on  
+        different univariate feature selection models from  scikit-learn.
+        :param n_features: SelectPercentile if float else SelectKBest  
+        :param problem_type: classification or regression  
+        :param scoring: scoring function, string 
+        """
+        if problem_type == 'classification':
+            valid_scoring = { 'f_classif': f_classif,
+                             'chi2': chi2,
+                             'mutual_info_classif': mutual_info_classif}
+        else:
+            valid_scoring = {'f_regression': f_regression,
+                             'mutual_info_regression': mutual_info_regression}
+        if scoring not in valid_scoring:
+            raise Exception("Invalid Scoring Function")
+        
+        if isinstance(n_features, int):
+            self.selection = SelectKBest(valid_scoring[scoring],
+                                         k = n_features)
+        elif isinstance(n_features, float):
+            self.selection = SelectPercentile(valid_scoring[scoring],
+                                              percentile = int(n_features*100))
+        else:
+            raise Exception("Invalid type of Feature")
+    
+    def fit(self,X,y):
+        return self.selection.fit(X,y)
+    
+    def transform(self,X):
+        return self.selection.transform(X)
+    
+    def fit_traansform(self,X,y):
+        return self.fit_transform(X,y)
+    
